216fd90d76cfefbd173c386e28056072b572b533
Hugh Simpson <hsimpson@rzsoftware.com>
2024-03-14 15:20:20 +0000

    more tests

--- build.sbt
+++ build.sbt
@@ -1971,7 +1971,9 @@ lazy val openapiCodegenCore: ProjectMatrix = (projectMatrix in file("openapi-cod
       scalaOrganization.value % "scala-reflect" % scalaVersion.value,
       scalaOrganization.value % "scala-compiler" % scalaVersion.value % Test,
       "com.beachape" %% "enumeratum" % "1.7.3" % Test,
-      "com.beachape" %% "enumeratum-circe" % "1.7.3" % Test
+      "com.beachape" %% "enumeratum-circe" % "1.7.3" % Test,
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.28.2" % Test,
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.28.2" % Provided
     )
   )
   .dependsOn(core % Test, circeJson % Test)
--- openapi-codegen/core/src/test/scala/sttp/tapir/codegen/BasicGeneratorSpec.scala
+++ openapi-codegen/core/src/test/scala/sttp/tapir/codegen/BasicGeneratorSpec.scala
@@ -3,49 +3,51 @@ package sttp.tapir.codegen
 import sttp.tapir.codegen.testutils.CompileCheckTestBase
 
 class BasicGeneratorSpec extends CompileCheckTestBase {
+  Seq("circe", "jsonit") foreach { jsonSerdeLib =>
 
-  it should "generate the bookshop example" in {
-    BasicGenerator.generateObjects(
-      TestHelpers.myBookshopDoc,
-      "sttp.tapir.generated",
-      "TapirGeneratedEndpoints",
-      targetScala3 = false,
-      useHeadTagForObjectNames = false,
-      jsonSerdeLib = "circe"
-    )("TapirGeneratedEndpoints") shouldCompile ()
-  }
+    it should "generate the bookshop example" in {
+      BasicGenerator.generateObjects(
+        TestHelpers.myBookshopDoc,
+        "sttp.tapir.generated",
+        "TapirGeneratedEndpoints",
+        targetScala3 = false,
+        useHeadTagForObjectNames = false,
+        jsonSerdeLib = jsonSerdeLib
+      )("TapirGeneratedEndpoints") shouldCompile()
+    }
 
-  it should "split outputs by tag if useHeadTagForObjectNames = true" in {
-    val generated = BasicGenerator.generateObjects(
-      TestHelpers.myBookshopDoc,
-      "sttp.tapir.generated",
-      "TapirGeneratedEndpoints",
-      targetScala3 = false,
-      useHeadTagForObjectNames = true,
-      jsonSerdeLib = "circe"
-    )
-    val schemas = generated("TapirGeneratedEndpoints")
-    val endpoints = generated("Bookshop")
-    // schema file on its own should compile
-    schemas shouldCompile ()
-    // schema file should contain no endpoint definitions
-    schemas.linesIterator.count(_.matches("""^\s*endpoint""")) shouldEqual 0
-    // Bookshop file should contain all endpoint definitions
-    endpoints.linesIterator.count(_.matches("""^\s*endpoint""")) shouldEqual 3
-    // endpoint file depends on schema file. For simplicity of testing, just strip the package declaration from the
-    // endpoint file, and concat the two, before testing for compilation
-    (schemas + "\n" + (endpoints.linesIterator.filterNot(_ startsWith "package").mkString("\n"))) shouldCompile ()
-  }
+    it should "split outputs by tag if useHeadTagForObjectNames = true" in {
+      val generated = BasicGenerator.generateObjects(
+        TestHelpers.myBookshopDoc,
+        "sttp.tapir.generated",
+        "TapirGeneratedEndpoints",
+        targetScala3 = false,
+        useHeadTagForObjectNames = true,
+        jsonSerdeLib = jsonSerdeLib
+      )
+      val schemas = generated("TapirGeneratedEndpoints")
+      val endpoints = generated("Bookshop")
+      // schema file on its own should compile
+      schemas shouldCompile()
+      // schema file should contain no endpoint definitions
+      schemas.linesIterator.count(_.matches("""^\s*endpoint""")) shouldEqual 0
+      // Bookshop file should contain all endpoint definitions
+      endpoints.linesIterator.count(_.matches("""^\s*endpoint""")) shouldEqual 3
+      // endpoint file depends on schema file. For simplicity of testing, just strip the package declaration from the
+      // endpoint file, and concat the two, before testing for compilation
+      (schemas + "\n" + (endpoints.linesIterator.filterNot(_ startsWith "package").mkString("\n"))) shouldCompile()
+    }
 
-  it should "compile endpoints with enum query params" in {
-    BasicGenerator.generateObjects(
-      TestHelpers.enumQueryParamDocs,
-      "sttp.tapir.generated",
-      "TapirGeneratedEndpoints",
-      targetScala3 = false,
-      useHeadTagForObjectNames = false,
-      jsonSerdeLib = "circe"
-    )("TapirGeneratedEndpoints") shouldCompile ()
+    it should "compile endpoints with enum query params" in {
+      BasicGenerator.generateObjects(
+        TestHelpers.enumQueryParamDocs,
+        "sttp.tapir.generated",
+        "TapirGeneratedEndpoints",
+        targetScala3 = false,
+        useHeadTagForObjectNames = false,
+        jsonSerdeLib = jsonSerdeLib
+      )("TapirGeneratedEndpoints") shouldCompile()
+    }
   }
 
 }
